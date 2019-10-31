e7f0d65732f9f81aec0089152d37b9ada0de3b50
saiaakash <p.saiaakash518@gmail.com>
2019-10-31 13:19:51 +0530

    Created seperate fragments

--- app/src/main/java/com/example/simpleattendence/MainActivity.kt
+++ app/src/main/java/com/example/simpleattendence/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.simpleattendence
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    supportFragmentManager.beginTransaction().add(R.id.fragmentPlaceHolder,StartPage()).commit()
+
+    }
+
+    fun StudentSignupPage(){
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentPlaceHolder,StudentSignup()).commit()
+
     }
 }
